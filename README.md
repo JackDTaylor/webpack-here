f7ecdd7f7a5c2869a63cd8872b8884a0123c99cf
Jack Taylor <msec.nt@gmail.com>
2019-06-23 10:45:38 +0300

    Add user config and implement override system, update readme.

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ It is recommended to install **webpack-here** globally:
 ```npm install -g webpack-here```
 
 ### Usage
-```wepack-here [source-file [destination-file]] [--publicPath=/public/path/]```
+```wepack-here [source-file [destination-file]] [--publicPath=/public/path/] [--customConfig=.webpack-here.config.js]```
 
 ### Usage with IDE's autoupload feature
 If you're uploading files on save, the resulting `{your-project}.js` file won't 
@@ -36,4 +36,71 @@ To workaround this issue in PHPStorm I've wrote
 to use with ScriptMonkey extension.
 
 ### Minification and sourcemaps
-I intentionally turned minification and sourcemaps off to reduce build time. Configuration options are planned but not implemented yet.
+I intentionally turned minification and sourcemaps off to reduce build time. 
+You can override it in your local `.webpack-here.config.js` file.
+
+### Public path for `babel-plugin-dynamic-import-webpack`
+If you want to use dynamic imports, you need to make sure `output.publicPath` option is correctly.
+
+By default `webpack-here` tries to assume a public path using your Git root or IntelliJ IDEA project root.
+Otherwise it will use `/` as public path.
+
+You can override public path by passing a `--publicPath` argument to to `webpack-here` like so:
+```bash
+webpack-here --publicPath=/my/public/path
+```
+
+### User config
+If you need to override some config options, you can create a `.webpack-here.config.js`
+file in your root directory. You can change the file name by passing `--customConfig` argument to `webpack-here` like so:
+```bash
+webpack-here --customConfig=relative/path/to/my-webpack-here-config.js
+```
+
+User config should export an object with the following structure:
+```
+module.exports = {
+	<SomeOverrideKey>: <someValue>,
+	<OtherOverrideKey>: <someOtherValue>,
+}
+```
+**Override key** is a string with dot-separated path to override, e.g. `"module.rules.0.test"`.
+You can end key with `[]` to merge instead of replace, e.g. `"module.rules[]"`. Merge type (object 
+or array) is determined by original value type (or by your value type if original doesn't exist).
+
+Here're some examples:
+```javascript
+module.exports = {
+	// Replace babel-loader without touching other params:
+	"module.rules.0.loader": "custom-babel-loader",
+	
+	// Replace babel-loader plugins completely:
+	"module.rules.0.query.plugins": ["@babel/plugin-the-only-plugin-i-need"],
+	
+	// Add babel-loader plugin:
+	"module.rules.0.query.plugins[]": [ "@babel/plugin-some-additional-plugin" ],
+	
+	// Add second loader without touching Babel:
+	"module.rules[]": [ { test: /\.(xls|xlsx|csv)$/, loader: "my-custom-spreadsheet-loader" } ],
+	
+	// Turn on error details without changing other `stats` options:
+	"stats.errorDetails": true,
+	
+	// If you don't need a deep merge, just treat this object like a regular webpack config:
+	cache: false,
+	devtool: "eval-cheap-module-source-map",
+	entry: [
+		'path/to/some-entry-file.js',
+		'path/to/some-other-entry-file.js'
+	],
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				sourceMap: true,
+				uglifyOptions: {
+					compress: { inline: false, drop_console: true },
+				},
+			}),
+		],
+	},
+};
--- src/WebpackHere.js
+++ src/WebpackHere.js
@@ -36,12 +36,12 @@ class WebpackHere {
 		console.log('');
 	}
 
-	get assertedPublicPath() {
+	get assumedPublicPath() {
 		let dir = this.cwd;
 		let subdirs = [];
 
 		while(Path.basename(dir)) {
-			if(FS.existsSync(Path.join(dir, '.idea'))) {
+			if(FS.existsSync(Path.join(dir, '.idea')) || FS.existsSync(Path.join(dir, '.git'))) {
 				return '/' + subdirs.reverse().join('/');
 			}
 
@@ -58,7 +58,11 @@ class WebpackHere {
 	get outputFile() { return (this.args[1] || `${Path.basename(this.cwd)}.js`); }
 
 	get publicPath() {
-		return this.namedArgs['publicPath'] || this.assertedPublicPath;
+		return this.namedArgs['publicPath'] || this.assumedPublicPath;
+	}
+
+	get customConfigFile() {
+		return this.namedArgs['customConfig'] || '.webpack-here.config.js';
 	}
 
 	get nodeModules() {
@@ -84,6 +88,59 @@ class WebpackHere {
 
 	get webpackHereConfigParams() {
 		return ['--env.webpackHereConfig=' + JSON.stringify(this.webpackHereConfig)];
+	}
+
+	get baseConfig() {
+		return {
+			mode: 'none',
+			cache: {},
+			devtool: false,
+			resolve: {
+				modules: ['node_modules', this.nodeModules],
+				extensions: [".jsx", ".webpack.js", ".web.js", ".js", ".json"],
+			},
+			resolveLoader: {
+				modules: ['node_modules', this.nodeModules],
+			},
+			watchOptions: {
+				ignored: [`/node_modules/`],
+			},
+			plugins: [
+				new Webpack.HashedModuleIdsPlugin(),
+				new LogOnCompile('WebpackHere'),
+			],
+			stats: {
+				all: false,
+				colors: true,
+				errors: true,
+				performance: true,
+				timings: false,
+				warnings: true,
+			},
+			module: {
+				rules: [{
+					test: /.jsx?$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					query: {
+						presets: [
+							require("@babel/preset-react"),
+							require("@babel/preset-env"),
+						],
+						plugins: [
+							require('babel-plugin-dynamic-import-webpack'),
+							require('@babel/plugin-external-helpers'),
+							require('@babel/plugin-syntax-async-generators'),
+							[require("@babel/plugin-proposal-decorators"), { legacy: true }],
+							[require('@babel/plugin-proposal-class-properties'), { loose : true }],
+							require('@babel/plugin-proposal-object-rest-spread'),
+							require('@babel/plugin-transform-regenerator'),
+							require('@babel/plugin-transform-runtime'),
+						]
+					}
+				}]
+			}
+		};
 	}
 
 	runInstance() {
@@ -125,89 +182,139 @@ class WebpackHere {
 		console.log('\twebpack-here, wh');
 		console.log('\tThis is a default usage. It will generate a `dirname.js` file with entry of `dirname.jsx`');
 		console.log('');
-		console.log('\twebpack-here [entryFile.jsx] [outputFile.js] [--publicPath=/public/path/]');
+		console.log('\twebpack-here [entryFile.jsx] [outputFile.js] [--publicPath=/public/path/] [--customConfig=.webpack-here.config.js]');
 		console.log('\tThis command will generate a `outputFile.js` file with entry of `entryFile.jsx`');
 	}
 
-	generateConfig(env) {
+	generateLocalOverride(env) {
 		const cfg = JSON.parse(env.webpackHereConfig);
 
-		return this.applyUserConfig({
-			mode: 'none',
-			entry: `${this.cwd}\\${cfg.entryFile}`,
-			output: {
+		return {
+			"entry": `${this.cwd}\\${cfg.entryFile}`,
+			"output": {
 				path: this.cwd,
 				filename: cfg.outputFile,
 				chunkFilename: cfg.outputFile.replace(/\.js$/, '.[name].js'),
 				publicPath: cfg.publicPath,
 			},
-			cache: {},
-			devtool: false,
-			resolve: {
-				modules: ['node_modules', this.nodeModules],
-				extensions: [".jsx", ".webpack.js", ".web.js", ".js", ".json"],
-			},
-			resolveLoader: {
-				modules: ['node_modules', this.nodeModules],
-			},
-			watchOptions: {
-				ignored: [`/node_modules/`],
-			},
-			plugins: [
-				new Webpack.HashedModuleIdsPlugin(),
-				new LogOnCompile('WebpackHere'),
-				new NotifyPHPStormOnCompile(`${this.cwd}\\${cfg.outputFile}`),
-			],
-			stats: {
-				all: false,
-				colors: true,
-				errors: true,
-				// errorDetails: true,
-				performance: true,
-				timings: false,
-				warnings: true,
-			},
-			module: {
-				rules: [{
-					test: /.jsx?$/,
-					loader: 'babel-loader',
-					exclude: /node_modules/,
-					query: {
-						presets: [
-							require("@babel/preset-react"),
-							require("@babel/preset-env"),
-						],
-						plugins: [
-							require('babel-plugin-dynamic-import-webpack'),
-							require('@babel/plugin-external-helpers'),
-							require('@babel/plugin-syntax-async-generators'),
-							[require("@babel/plugin-proposal-decorators"), { legacy: true }],
-							[require('@babel/plugin-proposal-class-properties'), { loose : true }],
-							require('@babel/plugin-proposal-object-rest-spread'),
-							require('@babel/plugin-transform-regenerator'),
-							require('@babel/plugin-transform-runtime'),
-						]
-					}
-				}]
-			}
-		});
+			"plugins.[]": [new NotifyPHPStormOnCompile(`${this.cwd}\\${cfg.outputFile}`)],
+		};
 	}
 
-	applyUserConfig(baseConfig) {
-		const userConfigFile = `${this.cwd}/.webpack-here.config.js`;
+	generateConfig(env) {
+		let config;
 
-		if(!FS.existsSync(userConfigFile)) {
-			return baseConfig;
+		// Generate base config with defaut overrides (entry, output)
+		config = this.baseConfig;
+		config = this.applyOverride(config, this.generateLocalOverride(env));
+
+		const customConfigFile = `${this.cwd}/${this.customConfigFile}`;
+
+		if(FS.existsSync(customConfigFile)) {
+			console.log('Applying custom config override from', customConfigFile);
+
+			// We've got user override -- apply it
+			config = this.applyOverride(config, require(customConfigFile));
 		}
 
-		console.log('Applying user config from', userConfigFile);
+		return config;
+	}
 
-		const userConfig = require(userConfigFile);
+	/**
+	 * @typedef {String} OverridePath
+	 * Dot-separated list of keys, optionally can end with [] to merge instead of override subtree
+	 * Examples:
+	 *   * `"some.test.path"` - replace `config.some.test.path` with passed value
+	 *   * `"some.existingArrayProp[]"` - array-merge ([...original, ...passed]) passed value with `config.some.arrayProp` key
+	 *   * `"some.existingObjectProp[]"` - object-merge ({...original, ...passed}) passed value with `config.some.arrayProp` key
+	 *   * `"some.nonExistingProp"` - adds a key `nonExistingProp` into `config.some` object
+	 *   * `"some.nonExistingProp[]"` - same as above.
+	 *   * `"nonExistingLevelOneProp.some.deep.path"` - creates a subtree like `{some:{deep:path: passedValue }}` and adds it as `config.nonExistingLevelOneProp`.
+	 *
+	 * If original value exists, it determines a merge type. Otherwise new key will be added.
+	 */
+	/**
+	 * @typedef {Object.<OverridePath, any>} Override
+	 */
+	/**
+	 * Applies override to an object.
+	 *
+	 * @param {Object} config
+	 * @param {Override} override
+	 */
+	applyOverride(config = {}, override = {}) {
+		for(const path of Object.keys(override)) {
+			const keys = path.split('.');
 
-		return {
-			...baseConfig,
-			...userConfig,
-		};
+			let i = 0;
+			let subtree = config;
+			let value = override[path];
+
+			for(const originalKey of keys) {
+				const isLast = i++ === keys.length - 1;
+				const merge = /(.*)\[]$/.exec(originalKey);
+
+				let isMerge = false;
+				let key = originalKey;
+
+				if(isLast && merge) {
+					isMerge = true;
+					key = merge[1];
+				}
+
+				if(!key) {
+					throw new Error('Empty key was encountered in "' + path + '" key of user override');
+				}
+
+				if(key in subtree === false) {
+					// Check if the subtree should be an array.
+					// NOTE: `i` is already incremented, so we don't need `i + 1` to get next key
+
+					if(!isLast && /^\d+$/.test(keys[i])) {
+						// This is not the last element and the next key is a digit
+						subtree[key] = [];
+					} else if(isMerge && Array.isArray(value)) {
+						// This is the last element and we're in merge mode and value is an array
+						subtree[key] = [];
+					} else {
+						// Otherwise treat it like an object
+						subtree[key] = {};
+					}
+				}
+
+				if(!isLast) {
+					// We need to go deeper!
+					subtree = subtree[key];
+					continue;
+				}
+
+
+				if(!isMerge) {
+					// We're not in merge mode so just overwrite the value and bail out.
+					subtree[key] = value;
+					break;
+				}
+
+				// We're in merge mode, so we should determine merge type from `subtree[key]` type
+				if(Array.isArray(subtree[key])) {
+					// Merge as array
+					subtree[key] = [
+						...subtree[key],
+						...value,
+					];
+				} else {
+					// Merge as object
+					subtree[key] = {
+						...subtree[key],
+						...value,
+					};
+				}
+
+				break;
+			}
+		}
+
+		return config;
 	}
 
 	get isConfigMode() {
